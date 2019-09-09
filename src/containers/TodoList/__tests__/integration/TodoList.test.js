9ed55f9698836d9982bc82d840101917d70382ed
huanshenyi <txy1226052@gmail.com>
2019-09-09 23:49:40 +0900

    lesson10

--- src/containers/TodoList/__tests__/integration/TodoList.test.js
+++ src/containers/TodoList/__tests__/integration/TodoList.test.js
@@ -3,6 +3,12 @@ import { findTestWrapper } from '../../../../utils/testUtils'
 import TodoList from '../../TodoList'
 import store from '../../../../store'
 
+// 元のコードのタイマーを入れ替える
+// 実行するたびにfaketimerをリセット、しなければ、testの間に実行数が累計される
+beforeEach(() => {
+  jest.useFakeTimers()
+})
+
 it(`
   1.ユーザーinputでvalueを入力、
   2.ユーザーはEnterKeyを押す、
@@ -17,4 +23,30 @@ it(`
   const listItems = findTestWrapper(wrapper, 'list-item')
   expect(listItems.length).toBe(1)
   expect(listItems.at(0).text()).toContain(content)
+})
+
+// 非同期処理のテスト
+// it(`
+// 1. ページ開く時に、apiデータを取得
+// 2.apiデータをページに表示
+// `, () => {
+//   const wrapper = mount(TodoList, { store })
+//   // $nextTick->少し待つ、非同期コード実行させる
+//   wrapper.vm.$nextTick(() => {
+//     const listItems = findTestWrapper(wrapper, 'list-item')
+//     expect(listItems.length).toBe(2)
+//   })
+// })
+it(`
+1. ページ開く時に、5sを待つ、apiデータを取得
+2.apiデータをページに表示
+`, () => {
+  const wrapper = mount(TodoList, { store })
+  expect(setTimeout).toHaveBeenCalledTimes(1)
+  // 時間が動き出す
+  jest.runAllTimers()
+  wrapper.vm.$nextTick(() => {
+    const listItems = findTestWrapper(wrapper, 'list-item')
+    expect(listItems.length).toBe(2)
+  })
 })
